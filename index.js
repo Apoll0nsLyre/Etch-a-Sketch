528e18048efea555559c549b77b893343b54a670
Apoll0nsLyre <gougeenzo@gmail.com>
2023-03-17 13:27:37 +0100

    add buttons

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const container = document.querySelector('.container');
-let gridSize= 16;
-// Create 256 square divs and append them to the container
+let valueSize = document.querySelector('#value');
+valueSize.addEventListener('input', function() {
+    let val = parseFloat(valueSize.value);
+    console.log(typeof val, val);
+
+});
+let currentColor = 'black';
+let backgroundColor = 'white';
+let gridSize = valueSize;
+
 function createGrid(size) {
     container.style.gridTemplateColumns = 'repeat($(size),1fr)';
     container.style.gridTemplateRows = 'repeat($(size),1fr)';
     for (let i = 0; i < size * size; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
-    }
+        const gridElement = document.createElement('div');
+        gridElement.addEventListener('mousemove', colorGrid);
+        container.appendChild(gridElement);
+    };
 };
 
 createGrid(gridSize);
+
+function colorGrid(e){
+    if (e.buttons === 1) {
+    e.target.style.backgroundColor = currentColor;
+    }
+};
+
