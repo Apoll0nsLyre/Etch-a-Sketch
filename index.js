77fc53c96093974dc1dd872e72cbaed6b584a4e5
Apoll0nsLyre <gougeenzo@gmail.com>
2023-05-30 14:16:34 +0200

    add function to buttons

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const currentSize = 16;
 const grid = document.querySelector('.grid');
 const currentColor = 'black';
+let currentBackgroundColor = 'white';
+
+const clearButton = document.getElementById('clear');
+const sizeButton = document.getElementById('new-grid-size');
+const colorButton = document.getElementById('color');
+const backgroundColorButton = document.getElementById('backg-color');
+
+document.getElementById('colorPalette').style.display = 'none';
+
+colorButton.addEventListener('click', () => {
+    if (document.getElementById('colorPalette').style.display === 'none') {
+        document.getElementById('colorPalette').click();
+    } else {
+        document.getElementById('colorPalette').style.display = 'none';
+    }
+    color = document.getElementById('colorPalette').value;
+});
 
 let size = currentSize;
 let color = currentColor;
+let valueSize = document.getElementById("valueSize").value;
+let backgroundColor = 'white';
+
+clearButton.addEventListener('click', clearGrid);
+sizeButton.addEventListener('click', changeSize);
 
 function createGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -14,22 +36,43 @@ function createGrid(size) {
         gridElement.addEventListener('mouseover', changeColor);
         gridElement.addEventListener('mousedown', changeColor);
         grid.appendChild(gridElement);
-        }
+        };
 };
-// function createGrid(size) {
-//     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-//     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-//     for (let i = 0; i < size * size; i++) {
-//         const gridElement = document.createElement('div');
-//         gridElement.addEventListener('mousemove', changeColor);
-//         grid.appendChild(gridElement);
-//     }
-// }
+
+function changeBackgroundColor() {
+    colorPalette.addEventListener('input', () => {
+        backgroundColor = colorPalette.value;
+        clearGrid(backgroundColor);
+    });
+    clearGrid(backgroundColor);
+    colorPalette.click();
+};
 
 function changeColor(e) {
     if (e.buttons === 1)
-        e.target.style.backgroundColor = currentColor;
-}
+        e.target.style.backgroundColor = color;
+};
+
+function clearGrid(b) {
+    const gridArray = Array.from(grid.childNodes);
+    gridArray.forEach((element) => {
+        element.style.backgroundColor = b;
+    });
+    size = valueSize;
+};
+
+function changeSize() {
+    size = document.getElementById("valueSize").value;
+    createGrid(size);
+    clearGrid(backgroundColor);
+};
+
+function clearGrid() {
+    const gridArray = Array.from(grid.childNodes);
+    gridArray.forEach((element) => {
+        element.style.backgroundColor = backgroundColor;
+    });
+};
 
 createGrid(size);
 
